a07bdcb06276c1ac51559718b07b94749c7cbaf5
Ayman <ayman.attaallah@vodafone.com>
2022-10-10 13:37:02 +0200

    initial commit

--- Sources/AYCore/AYCore.swift
+++ Sources/AYCore/AYCore.swift
@@ -1,6 +1,24 @@
-public struct AYCore {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
+public class AYCore {
+    /// Converts hex color to UIColor.
+    /// - Parameters:
+    ///   - hexString: A string contains hex color.
+    ///   - alpha: Alpha wanted.
+    /// - Returns: A UIColor from the given hex.
+    class func colorFrom(hexString: String, with alpha: CGFloat = 1) -> UIColor {
+        let r, g, b: CGFloat
+        let offset = hexString.contains("#") ? 1 : 0
+        let start = hexString.index(hexString.startIndex, offsetBy: offset)
+        let hexColor = String(hexString[start...])
+        let scanner = Scanner(string: hexColor)
+        var hexNumber: UInt64 = 0
+        if scanner.scanHexInt64(&hexNumber) {
+            r = CGFloat((hexNumber & 0xff0000) >> 16) / 256
+            g = CGFloat((hexNumber & 0x00ff00) >> 8) / 256
+            b = CGFloat(hexNumber & 0x0000ff) / 256
+            return UIColor(red: r, green: g, blue: b, alpha: alpha)
+        }
+        return UIColor(red: 0, green: 0, blue: 0, alpha: alpha)
     }
 }
--- Tests/AYCoreTests/AYCoreTests.swift
+++ Tests/AYCoreTests/AYCoreTests.swift
@@ -2,10 +2,8 @@ import XCTest
 @testable import AYCore
 
 final class AYCoreTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(AYCore().text, "Hello, World!")
+    func testReturnCorrectColorFromHex() {
+        let expectedColor = AYCore.colorFrom(hexString: "#ff0000")
+        XCTAssertEqual(expectedColor, .red)
     }
 }
