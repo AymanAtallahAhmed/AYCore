d4a71f223d9ae215c0f87e10b853c9311934d5a6
Ayman <ayman.attaallah@vodafone.com>
2022-10-10 23:48:03 +0200

    update test case.

--- Tests/AYCoreTests/AYCoreTests.swift
+++ Tests/AYCoreTests/AYCoreTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class AYCoreTests: XCTestCase {
     func testReturnCorrectColorFromHex() {
-        let expectedColor = AYCore.colorFrom(hexString: "ff0000")
-        XCTAssertEqual(expectedColor, UIColor.red)
+        let expectedColor = AYCore.colorFrom(hexString: "000000")
+        XCTAssertEqual(expectedColor, UIColor.init(red: 0, green: 0, blue: 0, alpha: 1))
     }
 }
