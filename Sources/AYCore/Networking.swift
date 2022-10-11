567ddd580e44d8c27fc041b162562e4bd24cd97c
Ayman <ayman.attaallah@vodafone.com>
2022-10-11 16:23:28 +0200

    refactoring first network API and enable DI for easier testing.

--- Sources/AYCore/Networking.swift
+++ Sources/AYCore/Networking.swift
@@ -7,20 +7,36 @@
 
 import Foundation
 
+protocol NetworkSession {
+    func get(from url: URL, completion: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+    func get(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        let dataTask = dataTask(with: url) { data, _, error in
+            completion(data, error)
+        }
+        dataTask.resume()
+    }
+}
+
 extension AYCore {
     public class Networking {
         /// Manager class responsible for handling netowk calls.
         /// - Warning: This class must be created before using any public API.
         public class Manager {
             public init() {}
-            private let session = URLSession.shared
-            
+            internal var session: NetworkSession = URLSession.shared
+
+            /// Interacts with live internet to retreive data from the given url.
+            /// - Parameters:
+            ///   - url: A *URL* object you with to fetch data from.
+            ///   - completion: Returns a result object contains opential data or error while fetching. 
             func loadData(from url: URL, completion: @escaping (NetworkResult<Data>) -> Void) {
-                let task = session.dataTask(with: url) { data, response, error in
+                session.get(from: url) { data, error in
                     let result: NetworkResult = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completion(result)
                 }
-                task.resume()
             }
         }
 
--- Tests/AYCoreTests/AYNetworkingTests.swift
+++ Tests/AYCoreTests/AYNetworkingTests.swift
@@ -13,20 +13,30 @@ final class AYNetworkingTests: XCTestCase {
     func testLoadData() {
         let manager = AYCore.Networking.Manager()
         let expectation = XCTestExpectation(description: "Data called")
-        guard let url = URL(string: "https://raywenderlich.com") else {
-            XCTFail("Could not get a valid url")
-            return
-        }
-        
+        let sessionMock = NetworkSessionMock()
+        manager.session = sessionMock
+        let data = Data([1, 2, 3])
+        sessionMock.data = data
+        let url = URL(fileURLWithPath: "url")
+
         manager.loadData(from: url) { result in
             expectation.fulfill()
             switch result {
             case .success(let loadedData):
-                XCTAssertNotNil(loadedData, "loaded data is nil")
+                XCTAssertEqual(data, loadedData, "loaded data is nil")
             case .failure(let error):
                 XCTFail(error?.localizedDescription ?? "error forming invalid result")
             }
         }
         wait(for: [expectation], timeout: 4)
+    }
+}
+
+class NetworkSessionMock: NetworkSession {
+    var data: Data?
+    var error: Error?
+    
+    func get(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        completion(data, error)
     }
 }
